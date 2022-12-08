63e3055dea5f34598025d12701131b3e7b2a7502
archstation <ejovo13@yahoo.com>
2022-12-08 22:25:33 +0100

    add a variety of getter methods

--- inc/team.hpp
+++ inc/team.hpp
@@ -11,6 +11,10 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <random>
 
 namespace world_cup {
 
@@ -41,16 +45,24 @@ struct Record {
         , goals_against{__goals_against}
     {}
 
-    double win_rate() const { return wins / games; }
-    double lose_rate() const { return losses / games; }
-    double avg_goals() const { return goals / games; }
+    double win_rate() const { return (double) wins / games; }
+    double lose_rate() const { return (double) losses / games; }
+    double avg_goals() const { return (double) goals / games; }
+
+    friend Record operator+(const Record &lhs, const Record &rhs);
 };
 
 
 
+// auto create_teams(const std::string &csv_file) -> std::vector<Team>;
+
 class Team {
 
 public:
+
+    Team() 
+        : name_{""}
+    {}
 
     Team(std::string name, Record home, Record away, Record total) 
         : name_{name}
@@ -60,10 +72,29 @@ public:
     {}
 
     // Read in a csv file in an established format
-    // Team(const std::istream& csv_file);
     Team(const std::string &csv_line);
 
+    auto win_rate() const -> double { return total_.win_rate(); }
+    auto total_games() const -> int { return total_.games; }
+    auto goals_per_game() const -> double { return total_.avg_goals(); }
+    auto name() const -> std::string { return name_; }
+
     // read a comma seperated value file name
+    friend std::ostream &operator<<(std::ostream& os, const Team& team);
+
+    static auto load_teams(const std::string &csv_file) -> std::vector<Team>;
+
+    static auto get_teams() -> std::vector<Team> {
+
+        std::string filename = "world_cup_stats.csv";
+        return load_teams(filename);
+    }
+
+    static auto top_48() -> std::vector<Team>;
+
+    // static auto 
+
+
 
 private:
 
@@ -73,5 +104,6 @@ private:
     Record total_;
 
 };
+
 
 } // namespace world_cup
